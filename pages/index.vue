9105f8e9257c1ac2d0ec0ae896c1cca8911eb0a0
hamicch <hamichtech@gmail.com>
2021-03-18 00:44:18 +0100

    made additional adjustments to the listing page

--- pages/index.vue
+++ pages/index.vue
@@ -20,16 +20,16 @@
 
      <div class="container-fluid browsing-history">
        <div class="row">
-         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
+         <div v-for="(product, index) in products" :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
          <div class="history-box">
            <!-- Product image -->
            <a href="#" class="a-link-normal">
-             <img src="#" alt="" class="img-fluid">
+             <img :src="product.photo" alt="" class="img-fluid">
            </a>
            <!-- product title -->
            <div class="a-spacing-top-base asin-title">
              <span class="a-text-normal">
-               <div class="p13n-sc-truncated">Product title</div>
+               <div class="p13n-sc-truncated">{{ product.title }}</div>
              </span>
            </div>
            <!-- prduct rating -->
@@ -42,12 +42,12 @@
                <i class="fas fa-star"></i>
              </a>
              <span class="a-letter-space"></span>
-             <span class="a-color-tertiary a-size-small asin-reviews">(1342</span>
+             <span class="a-color-tertiary a-size-small asin-reviews">(1342)</span>
            </div>
            <!-- product price -->
            <div class="a-row">
              <span class="a-size-base a-color-price">
-               <span class="p13n-sc-price">$23</span>
+               <span class="p13n-sc-price">{{ product.price }}</span>
              </span>
            </div>
            <!-- product buttons -->
@@ -63,7 +63,21 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // asyncData is fetching data before nuxt app finish loading on the browser.
+  // It is good for SEO because the data will be loaded first.
+  async asyncData({ $axios }) {
+    try{
+      let response = await $axios.$get("http://localhost:2021/api/products");
+      console.log(response);
+      return {
+        products: response.products
+      };
+    }catch(error) {
+      console.log('error', error);
+    }
+  }
+}
 </script>
 
 <style>
